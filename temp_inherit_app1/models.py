d1e6ee989b1f874a7003e8e895b58b2fba363e99
Khushi <Khushipandey8292@gmail.com>
2025-01-17 19:19:07 +0530

    include tag

--- temp_inherit_app1/models.py
+++ temp_inherit_app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class Student(models.Model):
+    stuid=models.IntegerField()
+    stuname=models.CharField(max_length=40)
+    stuemail=models.EmailField(max_length=50)
+    stupass=models.CharField(max_length=50)
 # Create your models here.
--- temp_inherit_app2/views.py
+++ temp_inherit_app2/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 def course(request):
-    return render(request,'core1/course_django')
+    return render(request,'core1/course_django.html')
 # Create your views here.
